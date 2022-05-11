37836057e1810eee3ccd5c0587da945c0ff48580
yuuto <larrota.juancamilo@gmail.com>
2022-05-11 15:52:48 -0500

    Cracion de lista de decodificacion con los datos del mensaje listos para desencriptar

--- decodificador_frecuencia.py
+++ decodificador_frecuencia.py
@@ -24,8 +24,14 @@ def ordenarCaracteres(caracteres):
     # print(sorted(caracteres, key=lambda caracter : caracter.frecuencia))
     return caracteres
 
-codigo = "53‡‡†305))6*;4826)4‡.)4‡);806*;48†8¶60))85;1‡(;:‡*8†83(88)5*†;46(;88*96*;8)*‡(;485);5 *†2:*‡ (;4956*2(5*—4)8¶8*;4069285);)6†8)4‡‡;1 (‡9;48081;8:8‡1;48†85;4)485†528806 *81(‡9;48;(88;4(‡?34; 48)4‡;161;:188;‡?;"
-frequency = ["e","t","a","o","i","n","s","h","r","d","l","u","c","m","f","w","y","g","p","b","v","k","q","j","x","z"]
+def asignarCaracteres(caracteres,frecuencia):
+    for i in range(len(caracteres)):
+        caracteres[i].frecuencia = frecuencia[i]
+    return caracteres
 
-caracteres = dividirCaracteres(codigo)
+mensajeEncriptado = "53‡‡†305))6*;4826)4‡.)4‡);806*;48†8¶60))85;1‡(;:‡*8†83(88)5*†;46(;88*96*;8)*‡(;485);5 *†2:*‡ (;4956*2(5*—4)8¶8*;4069285);)6†8)4‡‡;1 (‡9;48081;8:8‡1;48†85;4)485†528806 *81(‡9;48;(88;4(‡?34; 48)4‡;161;:188;‡?;"
+frequency = ["e","s","t","u","d","i","a","b","c","f","f","h","j","k","l","m","n","o","p","q","r","v","w","x","y"]
+
+caracteres = dividirCaracteres(mensajeEncriptado)
 caracteres = ordenarCaracteres(caracteres)
+codigo = asignarCaracteres(caracteres,frequency)
