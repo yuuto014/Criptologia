5f86774c6a6a6376d16a9985b8789a04db147ec6
yuuto <larrota.juancamilo@gmail.com>
2022-05-11 14:19:09 -0500

    se determina la frecuencia de cada caracter

---
+++ decodificador_frecuencia.py
@@ -0,0 +1,21 @@
+def dividirCaracteres(mensaje):
+    caracteres = []
+    frecuencia = []
+    for m in mensaje:
+        if m not in caracteres:
+            caracteres.append(m)
+            frecuencia.append(1)
+        else:
+            frecuencia[caracteres.index(m)] += 1
+
+    for i in range(len(caracteres)):
+        print("[",caracteres[i],"],[",frecuencia[i],"]")
+    return caracteres
+        
+
+codigo = "53‡‡†305))6*;4826)4‡.)4‡);806*;48†8¶60))85;1‡(;:‡*8†83(88)5*†;46(;88*96*;8)*‡(;485);5 *†2:*‡ (;4956*2(5*—4)8¶8*;4069285);)6†8)4‡‡;1 (‡9;48081;8:8‡1;48†85;4)485†528806 *81(‡9;48;(88;4(‡?34; 48)4‡;161;:188;‡?;"
+frequency = ["e","t","a","o","i","n","s","h","r","d","l","u","c","m","f","w","y","g","p","b","v","k","q","j","x","z"]
+
+dividirCaracteres(codigo)
+
+
