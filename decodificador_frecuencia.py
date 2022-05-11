e33a4ae348ad8d169b1b97278eb2023f958931d9
yuuto <larrota.juancamilo@gmail.com>
2022-05-11 15:18:55 -0500

    lista de caracteres ordenados por frecuencia

--- decodificador_frecuencia.py
+++ decodificador_frecuencia.py
@@ -18,9 +18,14 @@ def dividirCaracteres(mensaje):
         lista.append(Caracter(caracteres[i],frecuencia[i]))
         # print("[",caracteres[i],"],[",frecuencia[i],"]")
     return lista
-        
+
+def ordenarCaracteres(caracteres):
+    caracteres = sorted(caracteres, key=lambda caracter : caracter.frecuencia, reverse=True)
+    # print(sorted(caracteres, key=lambda caracter : caracter.frecuencia))
+    return caracteres
 
 codigo = "53‡‡†305))6*;4826)4‡.)4‡);806*;48†8¶60))85;1‡(;:‡*8†83(88)5*†;46(;88*96*;8)*‡(;485);5 *†2:*‡ (;4956*2(5*—4)8¶8*;4069285);)6†8)4‡‡;1 (‡9;48081;8:8‡1;48†85;4)485†528806 *81(‡9;48;(88;4(‡?34; 48)4‡;161;:188;‡?;"
 frequency = ["e","t","a","o","i","n","s","h","r","d","l","u","c","m","f","w","y","g","p","b","v","k","q","j","x","z"]
 
 caracteres = dividirCaracteres(codigo)
+caracteres = ordenarCaracteres(caracteres)
